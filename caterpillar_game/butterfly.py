63541481231f9aa112d17cf2111ec171d087477c
Petr Viktorin <encukou@gmail.com>
2020-03-25 01:31:04 +0100

    Make it all fast enough for a hundred butterflies

--- caterpillar_game/butterfly.py
+++ caterpillar_game/butterfly.py
@@ -1,64 +1,116 @@
 import random
 import math
+import time
 
 import pyglet
 
-from .resources import get_butterfly_image, get_image, BUTTERFLY_ANCHORS, WING_COUNT
-from .wing import get_wing_image
+from .resources import get_butterfly_image, get_image
+from .resources import BUTTERFLY_ANCHORS, WING_COUNT, BUTTERFLY_HEIGHT
+from .wing import start_wing_generation, get_wing_image
 
 BODY_COLOR = (61, 43, 6)
 
 class Demo:
     def __init__(self):
-        self.butterflies = [Butterfly([
-            random.uniform(0, 1)
-            for i in range(WING_COUNT)
-        ]) for i in range(1)]
+        self.butterflies = [
+            Butterfly(
+                [
+                    random.uniform(0, 1)
+                    for i in range(WING_COUNT)
+                ],
+                x=512, y=356,
+            )
+        ]
         self.bg = pyglet.sprite.Sprite(get_image('solid'))
         self.bg.color = 200, 200, 200
         self.bg.scale = 100
         self.t = 0
 
+        coords = [(x, y) for x in range(10) for y in range(10)]
+        random.shuffle(coords)
+        for x, y in coords:
+            butterfly = Butterfly(
+                [
+                    random.uniform(0, 1)
+                    for i in range(WING_COUNT)
+                ],
+                scale=0.1,
+                x=x*102+51, y=y*57+35,
+            )
+            self.butterflies.append(butterfly)
+
     def tick(self, dt):
+        if dt > 0.5:
+            return
         self.t += dt
 
     def draw(self):
         self.bg.draw()
-        for butterfly in self.butterflies:
-            butterfly.draw(max(-1, min(self.t-1.1, 0)))
+        for i, butterfly in enumerate(reversed(self.butterflies)):
+            butterfly.draw(self.t + i * 1/9 * (1 + i*0.01), partial=i%7==0)
 
 
 class Butterfly:
-    def __init__(self, hues):
+    def __init__(self, hues, x=0, y=0, scale=1):
         self.wing_batch = pyglet.graphics.Batch()
         self.body_batch = pyglet.graphics.Batch()
         self.sprites = []
-        sprite = pyglet.sprite.Sprite(
-            get_wing_image(hues),
-            batch=self.wing_batch,
-        )
-        self.size = sprite.height
-        self.sprites.append(sprite)
+        self.wing_sprite = None
         for name in 'abdomen', 'thorax', 'head', 'antenna', 'eye':
             sprite = pyglet.sprite.Sprite(
                 get_butterfly_image(name),
                 batch=self.body_batch,
             )
-            sprite.y = (BUTTERFLY_ANCHORS['wing']-BUTTERFLY_ANCHORS[name])*self.size
+            sprite.y = (
+                BUTTERFLY_ANCHORS['wing']-BUTTERFLY_ANCHORS[name]
+            ) * BUTTERFLY_HEIGHT
             sprite.color = BODY_COLOR
             self.sprites.append(sprite)
+        self.scale = scale
+        self.x = x
+        self.y = y
+        self.wing_gen = start_wing_generation(hues)
+        self.alive_since = None
 
-    def draw(self, t=0):
-        scale = abs(math.cos(t*math.tau/4))**3 * 0.99 + 0.01
-        x_wing = BUTTERFLY_ANCHORS['x-wing'] * self.size
+    @property
+    def is_done(self):
+        return self.wing_sprite or get_wing_image(self.wing_gen)
+
+    @property
+    def age(self):
+        if self.alive_since is None:
+            return 0
+        return time.time() - self.alive_since
+
+    def draw(self, t=1, partial=False):
+        t = t % 2
+        if t > 1:
+            t = 2 - t
+        if not self.wing_sprite:
+            image = get_wing_image(self.wing_gen)
+            if image is None:
+                if not partial:
+                    return
+            else:
+                self.wing_sprite = pyglet.sprite.Sprite(
+                    image, batch=self.wing_batch,
+                )
+                self.sprites.append(self.wing_sprite)
+                self.alive_since = time.time()
+        age = self.age
+        if self.age < 1:
+            t = min(self.age, t)
+        wing_scale = abs(math.sin(t*math.tau/4))**3 * 0.99 + 0.01
+        x_wing = BUTTERFLY_ANCHORS['x-wing'] * BUTTERFLY_HEIGHT
         pyglet.gl.glPushMatrix()
         try:
-            pyglet.gl.glTranslatef(512, 356, 0)
+            pyglet.gl.glTranslatef(self.x, self.y, 0)
+            pyglet.gl.glScalef(self.scale, self.scale, 1)
             self.body_batch.draw()
             pyglet.gl.glTranslatef(x_wing, 0, 0)
-            pyglet.gl.glScalef(scale, 1, 1)
+            pyglet.gl.glScalef(wing_scale, 1, 1)
             self.wing_batch.draw()
-            pyglet.gl.glTranslatef(-2 * x_wing / scale, 0, 0)
+            pyglet.gl.glTranslatef(-2 * x_wing / wing_scale, 0, 0)
             pyglet.gl.glScalef(-1, 1, 1)
             self.wing_batch.draw()
         finally:
--- caterpillar_game/wing.py
+++ caterpillar_game/wing.py
@@ -2,6 +2,9 @@ import png
 import importlib.resources
 import colorsys
 import array
+import concurrent.futures
+import time
+import multiprocessing
 
 import pyglet
 import numpy
@@ -20,30 +23,57 @@ def get_wing_matrix():
     )
     wing_matrix = numpy.stack([wing_matrix] * 4, -1)
     wing_matrix = wing_matrix.transpose((1, 2, 0, 3))[::-1, ...]
-    return wing_matrix
+    return wing_matrix.astype('uint16')
 
 wing_matrix = get_wing_matrix()
+wing_alpha = wing_matrix.sum(axis=2)
+wing_where = wing_alpha > 0
 
+wing_size = wing_matrix.shape[0]
 
-def get_wing_image(hues):
+# Using threads for CPU-bound task (numpy number crunching);
+# use a relatively small number of threads
+try:
+    thread_count = multiprocessing.cpu_count() // 2
+except NotImplementedError:
+    thread_count = 0
+if thread_count > 5:
+    thread_count = 5
+if thread_count < 1:
+    thread_count = 1
+pool = concurrent.futures.ThreadPoolExecutor(max_workers=thread_count)
+
+def _get_array(hues):
+    time.sleep(0)
     hues = list(hues)
     while len(hues) < resources.WING_COUNT:
         hues.append(0)
     colors = numpy.array([
         tuple(int(c * 255) for c in colorsys.hsv_to_rgb(hue, 0.9, 1)) + (255,)
         for i, hue in zip(range(resources.WING_COUNT), hues)
-    ], dtype='uint8')
-    plain = get_wing_matrix()
-    size = plain.shape[0]
-    colored = numpy.multiply(plain, colors, dtype='uint16')
-    result = colored.sum(axis=2) // plain.sum(axis=2)
+    ], dtype='uint16')
+    size = wing_matrix.shape[0]
+    colored = numpy.multiply(wing_matrix, colors, dtype='uint16')
+    result = numpy.zeros((size, size, 4), dtype='uint16')
+    numpy.floor_divide(colored.sum(axis=2), wing_alpha, out=result, where=wing_where)
     result = numpy.nan_to_num(result, 0)
     result = result.reshape(size, size * 4)
     result = result.astype('uint8')
-    image = pyglet.image.ImageData(
-        size, size, 'RGBA',
-        (pyglet.gl.GLubyte * result.size)(*result.tobytes()),
-    )
-    image.anchor_y = int((1-resources.BUTTERFLY_ANCHORS['wing']) * size)
-    image.anchor_x = int((resources.BUTTERFLY_ANCHORS['x-wing']) * size)
-    return image
+    result = (pyglet.gl.GLubyte * result.size).from_buffer_copy(result.tobytes())
+    return result
+
+def get_wing_image(futures):
+    if futures[1].done():
+        return futures[1].result()
+    if futures[0].done():
+        image = pyglet.image.ImageData(
+            wing_size, wing_size, 'RGBA', futures[0].result(),
+        )
+        image.anchor_y = int((1-resources.BUTTERFLY_ANCHORS['wing']) * wing_size)
+        image.anchor_x = int((resources.BUTTERFLY_ANCHORS['x-wing']) * wing_size)
+        futures[1].set_result(image)
+        return image
+
+def start_wing_generation(hues):
+    future = pool.submit(_get_array, hues)
+    return future, concurrent.futures.Future()
