c53da6205c29f37a5af1f40584aed1dc0e2be671
Petr Viktorin <encukou@gmail.com>
2020-03-28 21:07:38 +0100

    Zoom if the window changes size

--- caterpillar_game/window.py
+++ caterpillar_game/window.py
@@ -1,5 +1,10 @@
 import pyglet
 
+from .util import pushed_matrix
+
+WIDTH = 1024
+HEIGHT = 576
+
 KEY_MAP = {
     pyglet.window.key.F: 'fullscreen',
     pyglet.window.key.S: 'screenshot',
@@ -15,7 +20,7 @@ KEY_MAP = {
 
 class Window(pyglet.window.Window):
     def __init__(self, initial_scene, **kwargs):
-        super().__init__(width=1024, height=576, resizable=True)
+        super().__init__(width=WIDTH, height=HEIGHT, resizable=True)
         self.scene = initial_scene
 
     def run(self, fps=30):
@@ -25,13 +30,25 @@ class Window(pyglet.window.Window):
     def on_draw(self):
         self.clear()
 
-        # Draw current scene
-        try:
-            self.scene.draw()
-        except:
-            import traceback
-            traceback.print_exc()
-            raise
+        if self.height / HEIGHT < self.width / WIDTH:
+            zoom = self.height / HEIGHT
+            translate_x = (self.width / zoom - WIDTH) / 2
+            translate_y = 0
+        else:
+            zoom = self.width / WIDTH
+            translate_x = 0
+            translate_y = (self.height / zoom - HEIGHT) / 2
+
+        with pushed_matrix():
+            # Draw current scene
+            pyglet.gl.glScalef(zoom, zoom, 1)
+            pyglet.gl.glTranslatef(translate_x, translate_y, 1)
+            try:
+                self.scene.draw()
+            except:
+                import traceback
+                traceback.print_exc()
+                raise
 
     def tick(self, dt):
         self.scene.tick(dt)
